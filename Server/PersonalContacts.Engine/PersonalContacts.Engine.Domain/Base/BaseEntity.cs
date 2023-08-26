a9f8abf18bee7169e0b2429d55f016ab483c9782
tchimev <73113380+tchimev@users.noreply.github.com>
2023-08-26 23:23:26 +0300

    add domain entity, validation

---
+++ Server/PersonalContacts.Engine/PersonalContacts.Engine.Domain/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using PersonalContacts.Engine.Domain.Interfaces;
+
+namespace PersonalContacts.Engine.Domain.Base
+{
+    public abstract class BaseEntity<TKey> : IBaseEntity
+    {
+        public TKey Id { get; internal set; }
+    }
+}
